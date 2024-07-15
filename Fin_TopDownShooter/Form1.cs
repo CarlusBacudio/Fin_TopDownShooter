fa31c39c6a31877c4f08ad4e79360f6323f00493
HydrogenOxygen2 <156073985+HydrogenOxygen2@users.noreply.github.com>
2024-07-15 09:42:55 +0800

    Add project files.

---
+++ Fin_TopDownShooter/Form1.cs
@@ -0,0 +1,64 @@
+﻿using AxWMPLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fin_TopDownShooter
+{
+    public partial class Shooter : Form
+    {
+        public Shooter()
+        {
+            InitializeComponent();
+            wplayer.URL = (@"C:\Users\63929\source\repos\Fin_TopDownShooter\Fin_TopDownShooter\Resources\terror-ambience-7003.mp3");
+            wplayer.controls.play();
+            axWindowsMediaPlayer1.Hide();
+
+        }
+
+        public static WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            this.Hide();
+            form.Show();
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            this.Hide();
+            Form3 form = new Form3();
+            form.ShowDialog();
+            form = null; 
+            this.Show();
+
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Shooter_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Shooter_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Fin_TopDownShooter/Form3.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Fin_TopDownShooter
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.MUSIC = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Sound = new System.Windows.Forms.PictureBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Sound)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(191, 151);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(527, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // MUSIC
+            // 
+            this.MUSIC.AutoSize = true;
+            this.MUSIC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MUSIC.Location = new System.Drawing.Point(78, 151);
+            this.MUSIC.Name = "MUSIC";
+            this.MUSIC.Size = new System.Drawing.Size(84, 25);
+            this.MUSIC.TabIndex = 2;
+            this.MUSIC.Text = "MUSIC";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(648, 405);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Help";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_Sound
+            // 
+            this.btn_Sound.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Sound.BackgroundImage = global::Fin_TopDownShooter.Properties.Resources.sound_on;
+            this.btn_Sound.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Sound.Location = new System.Drawing.Point(2, 428);
+            this.btn_Sound.Name = "btn_Sound";
+            this.btn_Sound.Size = new System.Drawing.Size(20, 20);
+            this.btn_Sound.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.btn_Sound.TabIndex = 1;
+            this.btn_Sound.TabStop = false;
+            this.btn_Sound.Click += new System.EventHandler(this.btn_Sound_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MUSIC);
+            this.Controls.Add(this.btn_Sound);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form3_FormClosing);
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Sound)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label MUSIC;
+        private System.Windows.Forms.PictureBox btn_Sound;
+        private System.Windows.Forms.Label label1;
+    }
+}
